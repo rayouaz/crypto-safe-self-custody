2317be9509a631bb096e49a54d663d6f0232517b
Rayan Ayouaz <rayouaz@gmail.com>
2024-01-20 09:59:00 +0100

    Update RestrictedFundsV2.sol

--- contracts/RestrictedFundsV2.sol
+++ contracts/RestrictedFundsV2.sol
@@ -1,7 +1,7 @@
 
 pragma solidity ^0.8.0;
 
-contract RestrictedFunds {
+contract RestrictedFundsV2 {
     address public owner;
     address public authorizedKey;
     uint public cancellationRequestTime = 0;
